3407b050ea533848d832ec4a3830ee8c9e0b7d4a
Aringo <kib1773@naver.com>
2023-01-16 18:02:56 +0900

    0116_signup 회원가입 제작중

--- app/src/main/java/com/example/q_plan/Firebase_User.java
+++ app/src/main/java/com/example/q_plan/Firebase_User.java
@@ -1,13 +1,49 @@
 package com.example.q_plan;
 
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class Firebase_User {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private String userName;
+    private String userAge;
     private String userId;
     private String userPw;
+    private String userPw2;
+    private String userEmail;
 
     public Firebase_User() {}
-    public Firebase_User(String id, String pw) {
+    public Firebase_User(String name, String age, String id, String pw, String pw2, String email) {
+        this.userName = name;
+        this.userAge = age;
         this.userId = id;
         this.userPw = pw;
+        this.userPw2 = pw2;
+        this.userEmail = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String name) {
+        this.userName = name;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String age) {
+        this.userAge = age;
     }
 
     public String getUserId() {
@@ -26,11 +62,42 @@ public class Firebase_User {
         this.userPw = pw;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId = '" + userId + "\'" +
-                ", userpw = '" + userPw + "\'" +
-                "}";
+    public String getUserPw2() {
+        return userPw2;
+    }
+
+    public void setUserPw2 (String pw2) {
+        this.userPw2 = pw2;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setuserEmail (String email) {
+        this.userEmail = email;
+    }
+
+
+    public void sign() {
+        HashMap user = new HashMap();
+        user.put("Name", getUserName());
+        user.put("Age", getUserAge());
+        user.put("Pw", getUserPw());
+        user.put("Email", getUserEmail());
+
+        db.collection("Users").document(getUserId()).set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        System.out.println("가입성공");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("가입실패");
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/q_plan/SignUp.java
+++ app/src/main/java/com/example/q_plan/SignUp.java
@@ -3,25 +3,25 @@ package com.example.q_plan;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.type.Date;
 
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class SignUp extends AppCompatActivity {
 
+    public boolean id_check = false;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
@@ -29,10 +29,29 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //아이디 확인
         findViewById(R.id.Button_cs_idcheck).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                EditText id = findViewById(R.id.EditText_cs_id);
+                db.collection("Users").get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (DocumentSnapshot d : task.getResult()) {
+                                        if (d.getId().equals(id.getText().toString())) {
+                                            id_check = false;
+                                            return;
+                                        }
+                                    }
+                                    id_check = true;
+                                }
+                                else {
+                                    Toast.makeText(SignUp.this, "인터넷 연결을 확인해주세요.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
         findViewById(R.id.Button_cs_end).setOnClickListener(new View.OnClickListener() {
@@ -43,33 +62,49 @@ public class SignUp extends AppCompatActivity {
         });
     }
 
-    public boolean sign_check() {
-        boolean result = true;
+    public void sign_end() {
+        String result = sign_check();
+
+        if (result.equals("")) {
+
+
+            Toast.makeText(SignUp.this, "회원가입 성공!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String sign_check() {
         //이거 줄일방법 없나
         ArrayList<EditText> edit = new ArrayList<>();
         edit.add(findViewById(R.id.TextView_cs_name));
-        edit.add(findViewById(R.id.EditText_cs_birth1));
-        edit.add(findViewById(R.id.EditText_cs_birth2));
-        edit.add(findViewById(R.id.EditText_cs_birth3));
+        edit.add(findViewById(R.id.EditText_cs_age));
         edit.add(findViewById(R.id.EditText_cs_id));
-        edit.add(findViewById(R.id.EditText_cs_pw));    //5
+        edit.add(findViewById(R.id.EditText_cs_pw));
         edit.add(findViewById(R.id.EditText_cs_pw2));
         edit.add(findViewById(R.id.EditText_cs_email));
 
+        //이름
+        if (edit.get(0).getText().toString().isEmpty()) //빈칸
+            return "이름";
 
+        //나이
+        if (edit.get(1).getText().toString().isEmpty())   //빈칸
+            return "나이";
 
+        //아이디
+        if (!id_check)
+            return "아이디";
+
+        //비번
+        if (edit.get(3).getText().toString().isEmpty())
+            return "비밀번호";
+        else if (!edit.get(3).getText().toString().equals(edit.get(4).getText().toString()))
+            return "비밀번호";
 
         //이메일
         Pattern p = Patterns.EMAIL_ADDRESS;
-        if (p.matcher(edit.get(7).getText().toString()).matches())    //형식 확인
+        if (p.matcher(edit.get(5).getText().toString()).matches())    //형식 확인
         {
             db.collection("Users").get()
-                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                        @Override
-                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-
-                        }
-                    })
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -78,13 +113,17 @@ public class SignUp extends AppCompatActivity {
                                     System.out.println(d.getData().get("PW"));
                                 }
                             }
+                            else {
+                                Toast.makeText(SignUp.this, "인터넷 연결을 확인해주세요.", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     });
         } else {
-            edit.get(7).setBackgroundTintList(ColorStateList.valueOf(Color.RED));
-            result = false;
+            //edit.get(7).setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+            return "이메일";
         }
 
-        return result;
+        return "";
     }
+
 }
