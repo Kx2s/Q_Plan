969a95dae22130e19cd502c900cc8784a2de30e8
changhyun Jang <ckdgusjang@naver.com>
2023-03-15 03:10:42 +0900

    j_4_2와 j_4_1연결

--- app/src/main/java/com/example/q_plan/j_4_1.java
+++ app/src/main/java/com/example/q_plan/j_4_1.java
@@ -1,18 +1,35 @@
 package com.example.q_plan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 
-public class j_4_1 extends AppCompatActivity {
-    //j_4_1.xml 오픈
+public class j_4_1 extends AppCompatActivity implements OnClickListener{
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        //j_4_1.xml 열기
         super.onCreate(savedInstanceState);
         setContentView(R.layout.j_4_1);
+
+        //'다음' 버튼 선언
+        Button nextbtn=(Button) findViewById(R.id.next_open);
+
+        //'다음' 버튼 함수
+        nextbtn.setOnClickListener(this);
     }
+        //'다음'버튼 클릭시 j_4_2.xml 열기
+        @Override
+        public void onClick(View v){
+            Intent intent= new Intent(this, j_4_2.class);
+            startActivity(intent);
+        }
     // 창현
 }
--- app/src/main/java/com/example/q_plan/j_4_2.java
+++ app/src/main/java/com/example/q_plan/j_4_2.java
@@ -1,4 +1,14 @@
 package com.example.q_plan;
 
-public class j_4_2 {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class j_4_2 extends AppCompatActivity {
+    //j_4_2.xml열기
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.j_4_2);
+    }
 }
