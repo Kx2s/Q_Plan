5720951889f9b7ff8379407a504b8cf9e8bab02c
changhyun Jang <ckdgusjang@naver.com>
2023-03-15 10:46:25 +0900

    j_4_2와 j_4_1연결2

--- app/src/main/java/com/example/q_plan/j_4_2.java
+++ app/src/main/java/com/example/q_plan/j_4_2.java
@@ -1,14 +1,45 @@
 package com.example.q_plan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class j_4_2 extends AppCompatActivity {
+public class j_4_2 extends AppCompatActivity implements OnClickListener{
+
+    @Override
     //j_4_2.xml열기
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.j_4_2);
+
+        //'이전'버튼 선언
+        Button formerbtn=(Button) findViewById(R.id.former_open);
+        //'다음'버튼 선언
+        Button nextbtn2=(Button) findViewById(R.id.next_open2);
+
+        //'이전' 버튼 함수
+        formerbtn.setOnClickListener(this);
+        //'다음' 버튼 함수
+        nextbtn2.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            //'이전'버튼 클릭시 j_4_1.xml 열기
+            case R.id.former_open:
+                Intent intent= new Intent(this, j_4_1.class);
+                startActivity(intent);
+                break;
+            //'다음'버튼 클릭시 보류
+            case R.id.next_open2:
+                break;
+        }
+    }
+    // 창현
 }
