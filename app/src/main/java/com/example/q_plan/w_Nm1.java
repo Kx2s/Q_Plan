43e19764ada2f8df3cdeef4321f91067336b6d66
WJH <wonjonghyeok97@gmail.com>
2023-03-21 23:13:27 +0900

    w- Intro 구현 주석처리 파일 이름변경

--- app/src/main/java/com/example/q_plan/w_Nm1.java
+++ app/src/main/java/com/example/q_plan/w_Nm1.java
@@ -15,7 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class w_Nm1 extends Fragment {
+public class w_Nm1 extends Fragment  {
 
     private View view;
     @Nullable
