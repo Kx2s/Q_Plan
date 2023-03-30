4d9366836b4b78e86623067b7938c3a9ec1e8cbc
kx2s <kib1773@naver.com>
2023-03-31 04:48:40 +0900

    K0331 병합

--- app/src/main/java/com/example/q_plan/k_getApi.java
+++ app/src/main/java/com/example/q_plan/k_getApi.java
@@ -6,13 +6,9 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,11 +17,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.text.ParseException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
-import io.grpc.internal.JsonParser;
 
 public class k_getApi extends AsyncTask<String, Void, String> {
 
@@ -71,7 +65,7 @@ public class k_getApi extends AsyncTask<String, Void, String> {
         return sb.toString();
     }
 
-    public void setJson(String str){
+    public void saveJson(String str, int ctype){
         JSONArray json = null;
         AssetManager manager = context.getAssets();
         String filename = "data.json";
@@ -85,7 +79,7 @@ public class k_getApi extends AsyncTask<String, Void, String> {
             dos.close();
             System.out.println("저장");
         } catch(Exception e) {
-            Log.i("결과", e.toString() + "Error");}
+            Log.i("결과", e + "Error");}
     }
 
 
@@ -93,26 +87,24 @@ public class k_getApi extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... params) {
         System.out.println(params);
         String api = "https://ojbineyaldsjpfu2yfyqraqplu0ctpbl.lambda-url.ap-northeast-2.on.aws/";
-        URL url = null;
         try {
-            url = new URL(api + "?" + querySet()); // 서버 URL
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-            conn.setRequestProperty("x-waple-authorization", clientKey);
+            for (Object i : Arrays.asList(12, 14, 15, 25, 28, 32, 38, 39)) {
+                URL url = new URL(api + "?" + querySet() + "&contenttypeid=" + i.toString()); // 서버 URL
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+                conn.setRequestProperty("x-waple-authorization", clientKey);
 
-            if (conn.getResponseCode() == conn.HTTP_OK) {
-                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
-                BufferedReader reader = new BufferedReader(tmp);
-                StringBuffer buffer = new StringBuffer();
-                while ((str = reader.readLine()) != null) {
-                    buffer.append(str);
+                if (conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    reader.close();
+                } else {
+                    Log.i("결과", conn.getResponseCode() + "Error");
                 }
-
-                setJson(buffer.toString());
-                reader.close();
-
-            } else {
-                Log.i("결과", conn.getResponseCode() + "Error");
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
