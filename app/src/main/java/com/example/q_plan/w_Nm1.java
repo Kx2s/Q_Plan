c22fec29cd1b50f9b7f9079524d9e023fc5ec347
changhyun Jang <ckdgusjang@naver.com>
2023-05-09 17:17:21 +0900

    구글 현재 위치 적용

--- app/src/main/java/com/example/q_plan/w_Nm1.java
+++ app/src/main/java/com/example/q_plan/w_Nm1.java
@@ -1,15 +1,27 @@
 package com.example.q_plan;
 
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,14 +38,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class w_Nm1 extends Fragment  implements OnMapReadyCallback {
+public class w_Nm1 extends Fragment  implements OnMapReadyCallback{
+    //구글맵 변수 선언
     private MapView googlemap=null;
     private View view;
+    //
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+    @SuppressLint("MissingPermission")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.w_nm1, container, false);
 
+        // 위치 권한이 있는지 확인
+        if(isLocationPermissionGranted()){
+            //위치관리자 객체 생성
+            LocationManager locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
+            //위치 제공자 설정
+            String locationProvider = LocationManager.GPS_PROVIDER;
+            //위치 업데이트 받을 시간
+            long minTime = 30 * 1000;
+            //위치 업데이트 받을 최소거리
+            float minDistance = 1000;
+            LocationListener locationListener=new LocationListener() {
+                //위치 업데이트
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    Log.d("Location", "Latitude: " + latitude + ", Longitude: " + longitude);
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+                @Override
+                public void onProviderEnabled(String provider) {}
+
+                @Override
+                public void onProviderDisabled(String provider) {}
+            };
+            locationManager.requestLocationUpdates(locationProvider, minTime, minDistance, locationListener);
+        }else {
+            // 위치 권한이 없는 경우
+            requestLocationPermission();
+        }
+
+
+        //구글맵 변수 초기화
         googlemap=(MapView)view.findViewById(R.id.map);
         googlemap.getMapAsync(this);
 
@@ -101,7 +153,6 @@ public class w_Nm1 extends Fragment  implements OnMapReadyCallback {
         super.onActivityCreated(savedInstanceState);
 
         //액티비티가 처음 생성될 때 실행되는 함수
-
         if(googlemap != null)
         {
             googlemap.onCreate(savedInstanceState);
@@ -109,7 +160,7 @@ public class w_Nm1 extends Fragment  implements OnMapReadyCallback {
     }
     @Override
     public void onMapReady(GoogleMap googleMap){
-        LatLng CheonAhn=new LatLng(36.909189,127.144390);
+        LatLng CheonAhn=new LatLng(36.909,127.144);
 
         //마커 옵션
         MarkerOptions marker = new MarkerOptions();
@@ -123,4 +174,55 @@ public class w_Nm1 extends Fragment  implements OnMapReadyCallback {
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
 
     }
+    //위치 권한 허용되었는지 확인
+    private boolean isLocationPermissionGranted() {
+        return ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+    //위치권한 요청
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(), // Fragment가 속한 Activity를 가져와서 사용
+                Manifest.permission.ACCESS_FINE_LOCATION
+        )) {
+            new AlertDialog.Builder(requireContext()) // Fragment가 속한 Context를 가져와서 사용
+                    .setTitle("Location permission needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton("OK", (dialog, which) -> {
+                        ActivityCompat.requestPermissions(
+                                requireActivity(), // Fragment가 속한 Activity를 가져와서 사용
+                                new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                                LOCATION_PERMISSION_REQUEST_CODE
+                        );
+                    })
+                    .create()
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(
+                    requireActivity(), // Fragment가 속한 Activity를 가져와서 사용
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE
+            );
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 위치 권한이 허용된 경우
+                LocationManager locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
+                String locationProvider = LocationManager.NETWORK_PROVIDER;
+                long minTime = 60 * 1000;
+                float minDistance = 1000;
+
+            } else {
+                // 위치 권한이 거부된 경우
+                Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
